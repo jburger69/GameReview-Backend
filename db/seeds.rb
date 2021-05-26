fc20eed352a6b9db465674d4e7e65cbcfcad1d2d
Joshua Burger <joshberger6969@outlook.com>
2021-05-26 01:08:45 -0400

    Generate 30 new games with 30 new reviews linked to each game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Game.delete_all
+
+30.times do
+    Game.create(
+        title: Faker::Game.title,
+        price: Faker::Number.between(from: 1, to: 100),
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+    )
+end
+
+30.times do
+    Review.create(
+        name: Faker::Name.name,
+        content: Faker::Lorem.sentence(word_count: 3),
+        game_id: rand(1..30),
+    )
+end
